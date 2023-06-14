2b7d9d266b5d97a9b619dbefb9f950a6f9979281
Lupe Aguilera <lupe.aguilera2002@gmail.com>
2023-06-14 01:09:27 -0300

    comit1

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+
+    var menuIcon = document.getElementById("menu-icon");
+    var linksNav = document.getElementById("links");
+
+    menuIcon.addEventListener("click", function() {
+        linksNav.classList.toggle("show");
+    });
